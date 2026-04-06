614cfe436e753231689ef6e2497b00c187504d50
mayuribhujbal16 <mayuribhujbal9893@gmail.com>
2026-04-06 17:57:10 +0530

    table per concrete

---
+++ TablePerConcreteDemo/src/main/java/test/Contract_emp.java
@@ -0,0 +1,23 @@
+package test;
+
+public class Contract_emp extends Employee
+{
+	private float pay_per_hours;
+	private String contrct_period;
+	
+	public float getPay_per_hours() {
+		return pay_per_hours;
+	}
+	public void setPay_per_hours(float pay_per_hours) {
+		this.pay_per_hours = pay_per_hours;
+	}
+	public String getContrct_period() {
+		return contrct_period;
+	}
+	public void setContrct_period(String contrct_period) {
+		this.contrct_period = contrct_period;
+	}
+	
+	
+
+}
---
+++ TablePerConcreteDemo/src/main/java/test/EmpDataAccess.java
@@ -0,0 +1,57 @@
+package test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class EmpDataAccess {
+
+	public static void main(String[] args) 
+	{
+
+
+		// Step-1 Reading details from hibernate configuration file
+		
+		Configuration conf = new Configuration();
+		conf.configure("hibernate.cfg.xml");
+		
+		//Step-2 
+		SessionFactory factory= conf.buildSessionFactory();
+		
+		//Step-3 
+		Session session = factory.openSession();  // session:create new session/column in ur session in ur database
+		
+		//Step-4 
+		Transaction tx = session.beginTransaction();
+		
+		//create persistance object 
+		
+		Employee e1 = new Employee();
+		e1.setName("Mayuri");
+		
+		Reg_emp e2=new Reg_emp();
+		e2.setName("Bunty");
+		e2.setSalary(40000);
+		e2.setBonus(5);
+		
+		Contract_emp e3= new Contract_emp();
+		e3.setName("Pradnya");
+		e3.setPay_per_hours(10);
+		e3.setContrct_period("15 Hours");
+		
+		session.persist(e1);
+		session.persist(e2);
+		session.persist(e3);
+		
+		tx.commit();
+		session.close();
+		factory.close();
+		
+		System.out.println("Details added Successfullyyyyyyy!!!");
+		
+
+
+	}
+
+}
